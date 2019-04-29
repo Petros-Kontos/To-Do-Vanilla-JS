0628713abad66a4a563b74be5d32536f5afb51d3
Petros Kontos <p_kontos@hotmail.com>
2019-04-29 13:07:39 +0100

    incomplete changes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var item = document.getElementById('item')
+//create textbox
+
+var item = document.getElementById('item');
 
 item.addEventListener("click", function(e) {
     item.classList.toggle("complete")
+});
+
+//save new entries
+
+var save = document.getElementById('save-btn');
+var entry = document.getElementById('entry').value;
+
+
+
+save.addEventListener("click", function(e) {
+    console.log(entry);
 });
