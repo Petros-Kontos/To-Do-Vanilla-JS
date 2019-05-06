56d258ad42d020d49630acfd0670e84bed7a387e
Petros Kontos <p_kontos@hotmail.com>
2019-05-06 20:18:09 +0100

    comments

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
 
-    li.appendChild(t);
+    //Store textbox input into variable
+
+    var inputValue = document.getElementById("myInput").value;
+    
+    //Alert for empty value or append value to list
 
     if (inputValue === '') {
         alert("You must write something!");
-      } else {
-        document.getElementById("myUL").appendChild(li);
+    } else {
+        var item = document.createElement("li");
+        item.innerText = inputValue;
+
+        document.getElementById("myUL").appendChild(item);
       }
       document.getElementById("myInput").value = "";
+
+      //Needs 'x' icon to be appended to every <li> as well
 }
+
+// Draw a line through list item when clicked
+// Problem: currently only works for <li> that already exist in the html, not for ones added via the textbox.
+// Possible solution: create a loop that scans the DOM and creates an array object.
+// That is then changed (its CSS) similar to how innerText changes HTML.
+
+var item = document.querySelector("li");
+
+    item.addEventListener('click', function(e) {
+        
+        item.classList.toggle('check')
+    })
+
+
+// Delete entry when 'x' icon is clicked.
